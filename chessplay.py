5d8c848ae656deec85d2d09c867d2a3e06ed7f53
luoj1 <zluo77@gatech.edu>
2019-04-20 01:56:19 -0400

    rookie mover

--- chessplay.py
+++ chessplay.py
@@ -18,33 +18,110 @@ R1 = 13
 R2 = 14
 Q = 16
 class Node:
-	def __init__(board):
-
-		self.status = board 
+	def __init__(board,status):
+		self.status = status
+		self.board = board 
 		self.children = []
 
 class ChessPlay:
+	'''
+	initialize the memory of board for player
+	initialize piece status tracker
+	'''
 	def __init__(self, color):
 		self.depth = 3
-		self.pieces_status = np.array([1 for _ in range(16)]) # 1 is alive. 0 is dead
-		self.current_board = numpy # init board with numpy
+		self.pieces_status = dict() 
+		self.current_board = np.zeros(8,8) # init board with numpy
+		#init as color 
+		if color == 'b':
+			for i in range(1,9):
+				current_board[1][i-1] = 'p'+str(i)
+				self.pieces_status['p'+str(i)]  = (1,i-1)
+			current_board[0][0] = 'r1'
+			current_board[0][7] = 'r2'
+			current_board[0][1] = 'n1'
+			current_board[0][6] = 'n2'
+			current_board[0][2] = 'b1'
+			current_board[0][5] = 'b2'
+			current_board[0][3] = 'q'
+			current_board[0][4] = 'k'
+			self.pieces_status['r1'] = (0,0)
+			self.pieces_status['r2'] = (0,7)
+			self.pieces_status['n1'] = (0,1)
+			self.pieces_status['n2'] = (0,6)
+			self.pieces_status['b1'] = (0,2)
+			self.pieces_status['b2'] = (0,5)
+			self.pieces_status['q'] = (0,3)
+			self.pieces_status['k'] = (0,4)
+		elif color == 'w':
+			for i in range(1,9):
+				current_board[6][i-1] = 'p'+i
+				self.pieces_status['p'+str(i)]  = (6,i-1)
+			current_board[7][0] = 'r1'
+			current_board[7][7] = 'r2'
+			current_board[7][1] = 'n1'
+			current_board[7][6] = 'n2'
+			current_board[7][2] = 'b1'
+			current_board[7][5] = 'b2'
+			current_board[7][3] = 'q'
+			current_board[7][4] = 'k'
+			self.pieces_status['r1'] = (7,0)
+			self.pieces_status['r2'] = (7,7)
+			self.pieces_status['n1'] = (7,1)
+			self.pieces_status['n2'] = (7,6)
+			self.pieces_status['b1'] = (7,2)
+			self.pieces_status['b2'] = (7,5)
+			self.pieces_status['q'] = (7,3)
+			self.pieces_status['k'] = (7,4)
+
 		self.aim_on_king = 0 #
 		self.color = color # color
+		self.killed = 0
+		self.dead_pos = None
+
+
+	def s2ind(uci_move_string):
+		return (int(uci_move_string[1]), ord(uci_move_string[0]) - 97)
+
+	def eliminate(loc):
+		l = self.s2ind(loc)
+		self.current_board[l[0]][l[1]] = 0
+		self.pieces_status = 0
 		self.killed = 1
-		self.dead_pos = 1
-	def form_board_node(self, pieceDistr, table_produced = 1):
+		self.dead_pos = l
+
+	def form_board_node(self, pieceDistr):
 		pieces = dict()
-		out = np.zeros(8,8)
+		out = np.array(self.current_board)
 		unassigned_pieces = []
+		enemy_status = dict()
+		pieceDistr = np.array(pieceDistr)
 		for p in pieceDistr:
-			ind = np.unravel_index(np.argmax(a, axis=None), a.shape)
+			ind = np.unravel_index(np.argmax(pieceDistr[p], axis=None), pieceDistr[p].shape)
 			pieces[p] = ind
 
 		for p in pieces:
+			p = 'e'+p
 			r = pieces[p][0]
 			c = pieces[p][1]
-			out[r][c] = p
-				
+			if out[r][c] == 0:
+				out[r][c] = p:
+				enemy_status[p] = (r,c)
+			else:
+				tmp = (r,c)
+				while not out[tmp[0]][tmp[1]] == 0: 
+					pieceDistr[p][tmp[0]][tmp[1]] = -1
+					ind = np.unravel_index(np.argmax(pieceDistr[p], axis=None), pieceDistr[p].shape)
+					tmp = ind
+				enemy_status[p] = tmp
+				out[tmp[0]][tmp[1]] = p
+
+		n = Node(out, enemy_status)
+		print(out)
+		print(enemy_status)
+		return n
+
+			
 		return collection
 	def scorer(self, condition, kill, side):
 		out = 0
@@ -53,13 +130,19 @@ class ChessPlay:
 			out += 5
 		return out
 
-	def init_tree(self, board):
+	def init_tree(self, possible_moves):
 		# init minmax tree with scorring
 		node = self.form_board_node(get_pieceDistr())
+		# match possible moves to pin
+
+
 		init_tree_helper():
 	def init_tree_helper(self,node,color):
-		if self.color == color: #my_condition
-			board = 
+		#if self.color == color: #my_condition
+			#board = 
+
+
+
 	def minimax(self):
 		#minimax on tree to get the best move
 		for m in range():
